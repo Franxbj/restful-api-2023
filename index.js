feb3cadaa611338ebe1fc59467c8ff132b3e37b6
Francis Boafo <97594225+Franxbj@users.noreply.github.com>
2023-04-13 11:00:42 +0300

    Added some error handing

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 //dummy 'database'
 let products = [   
     { id: 1, name: 'Chair', price: 100 },   
@@ -13,7 +16,7 @@ app.get('/api/products', (req, res) => {
     res.json(products);
 });
 
-// GET ON http://localhost:500/api/products/2
+// GET ONE http://localhost:5000/api/products/2
 app.get('/api/products/:id', (req, res) => {
     const id = Number(req.params.id);
     const product = products.find(product => product.id === id);
@@ -30,8 +33,50 @@ app.get('/api/products/:id', (req, res) => {
             }
         )
     }
-    //res.json(product);    
+    
 });
+
+// DELETE http://localhost:5000/api/products/2
+app.delete('/api/products/:id', (req,res)=> {
+    const id = Number(req.params.id);
+     
+    products = products.filter(product => product.id !== id);
+
+    res.json(products);
+    
+})
+
+
+// CREATE A PRODUCT http://localhost:5000/api/products
+app.post('/api/products', (req,res) => {
+    //console.log(req.body.name);
+    //res.send("ok");
+
+    if (!req.body.name || !req.body.price)
+    {
+        res.status(400).json(
+            { msg: 'Name or price was not sent'}
+        )
+    }
+    else 
+    {
+        const newId = products[products.length-1].id +1;
+
+        const newProduct = { 
+            id : newId,
+            name : req.body.name,
+            price : req.body.price
+        }
+    
+        products.push(newProduct);
+    
+        res.json(products); 
+    }
+    
+
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
